03d90b970ed8f6db8ffd5a4fd36960cf239dc9a6
RicardoJavierFernandez <rickyjf808@gmail.com>
2019-10-26 10:20:41 -0400

    Updates

---
+++ precache-manifest.fc9b790f83b08f474f6f20334aa10f98.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb94aa4f19a7e1c4d134159fa74a047",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a5864c908a308d883084",
+    "url": "/Clicky-Game/static/css/main.804bb538.chunk.css"
+  },
+  {
+    "revision": "c2dce28c6b28d652dd08",
+    "url": "/Clicky-Game/static/js/2.cb751e63.chunk.js"
+  },
+  {
+    "revision": "a5864c908a308d883084",
+    "url": "/Clicky-Game/static/js/main.701f46d3.chunk.js"
+  },
+  {
+    "revision": "8895b329250370fb97f6",
+    "url": "/Clicky-Game/static/js/runtime-main.8ac3e39b.js"
+  },
+  {
+    "revision": "e80510660bfcf4b06273ab12bce79e03",
+    "url": "/Clicky-Game/static/media/flamingo.e8051066.jpg"
+  },
+  {
+    "revision": "afc90f738c04df07efc90c1b4f9aed77",
+    "url": "/Clicky-Game/static/media/meerkat.afc90f73.jpg"
+  },
+  {
+    "revision": "815a01dca5aa45eabc07008d43315e94",
+    "url": "/Clicky-Game/static/media/panda.815a01dc.jpg"
+  },
+  {
+    "revision": "0e735323979103266ade0daffeb77911",
+    "url": "/Clicky-Game/static/media/peacock.0e735323.jpg"
+  },
+  {
+    "revision": "afed69f573b6f23163e8c228812bc2fc",
+    "url": "/Clicky-Game/static/media/squirrel.afed69f5.jpg"
+  },
+  {
+    "revision": "ec5ed519fa150ca8a9878f12f691fb4e",
+    "url": "/Clicky-Game/static/media/tiger.ec5ed519.jpg"
+  }
+]);
